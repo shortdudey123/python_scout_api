eac4f39039c4e8457f8934d55decb261d3eb0ffd
Grant Ridder <grant@teambanjo.com>
2015-06-02 18:13:38 -0700

    Move docstrings

--- scout_api/scout_api.py
+++ scout_api/scout_api.py
@@ -25,127 +25,127 @@ class ScoutAPI(object):
         self.api_key = api_key
         self.log = logging.getLogger(__name__)
 
-    """
-    Get the metric data based on the given filters.
-    More info about the metrics API endpoint can be found here:
-    http://help.scoutapp.com/v1.2/docs/api#fetching-metrics
-
-    Args:
-        metric_name (str): the non-humanized metric name
-        metric_type (str): disk|memory|network|process|plugin
-        metric_source_name (str): if plugin, the plugin name. If process, the process name. if disk|network, the volume/interface name
-        environment (str, optional): environment name (exact match required). DEFAULT: no filtering by environment
-        role (str, optional): role name (exact match required.) DEFAULT: no filtering by role
-        server (str, optional): server hostname (exact match, wildcard, or regex ok)
-        duration (str, optional): one of: "30 minutes", "5 hours", "12 hours", "1 day", "1 week", "2 weeks", "1 month", "3 months", "6 months", "1 year". DEFAULT: 5 hours
-        display (str, optional): range|breakout. DEFAULT: range
-
-    Returns:
-        dict: result from the api call in the below format
-
-        {u'metrics': [{u'label': u'hostname1',
-                       u'points': [[0.0, 0.0, 0.0],
-                                   [0.0, 0.0, 0.0],
-                                   [0.0, 0.0, 0.0],
-                                   [0.0, 0.0, 0.0],
-                                   [0.0, 0.0, 0.0],
-                                   [0.0, 0.0, 0.0],
-                                   [0.0, 0.0, 0.0],
-                                   [0.0, 0.0, 0.0],
-                                   [0.0, 0.0, 0.0],
-                                   [0.0, 0.0, 0.0],
-                                   [0.0, 0.0, 0.0],
-                                   [0.0, 0.0, 0.0],
-                                   [0.0, 0.0, 0.0],
-                                   [0.0, 0.0, 0.0],
-                                   [0.0, 0.0, 0.0],
-                                   [0.0, 0.0, 0.0],
-                                   [0.0, 0.0, 0.0],
-                                   [0.0, 0.0, 0.0],
-                                   [0.0, 0.0, 0.0],
-                                   [0.0, 0.0, 0.0],
-                                   [0.0, 0.0, 0.0],
-                                   [0.0, 0.0, 0.0],
-                                   [0.0, 0.0, 0.0],
-                                   [0.0, 0.0, 0.0],
-                                   [0.0, 0.0, 0.0],
-                                   [0.0, 0.0, 0.0],
-                                   [0.0, 0.0, 0.0],
-                                   [0.0, 0.0, 0.0],
-                                   [0.0, 0.0, 0.0],
-                                   [0.0, 0.0, 0.0]],
-                       u'precision': 2,
-                       u'units': u''},
-                      {u'label': u'hostname2',
-                       u'points': [[1.0, 1.0, 1.0],
-                                   [1.0, 1.0, 1.0],
-                                   [1.0, 1.0, 1.0],
-                                   [1.0, 1.0, 1.0],
-                                   [1.0, 1.0, 1.0],
-                                   [1.0, 1.0, 1.0],
-                                   [1.0, 1.0, 1.0],
-                                   [1.0, 1.0, 1.0],
-                                   [1.0, 1.0, 1.0],
-                                   [1.0, 1.0, 1.0],
-                                   [1.0, 1.0, 1.0],
-                                   [1.0, 1.0, 1.0],
-                                   [1.0, 1.0, 1.0],
-                                   [1.0, 1.0, 1.0],
-                                   [1.0, 1.0, 1.0],
-                                   [1.0, 1.0, 1.0],
-                                   [1.0, 1.0, 1.0],
-                                   [1.0, 1.0, 1.0],
-                                   [1.0, 1.0, 1.0],
-                                   [1.0, 1.0, 1.0],
-                                   [1.0, 1.0, 1.0],
-                                   [1.0, 1.0, 1.0],
-                                   [1.0, 1.0, 1.0],
-                                   [1.0, 1.0, 1.0],
-                                   [1.0, 1.0, 1.0],
-                                   [1.0, 1.0, 1.0],
-                                   [1.0, 1.0, 1.0],
-                                   [1.0, 1.0, 1.0],
-                                   [1.0, 1.0, 1.0],
-                                   [1.0, 1.0, 1.0]],
-                       u'precision': 2,
-                       u'units': u''}],
-         u'num_servers': 2,
-         u'step': 60,
-         u'timestamps': [1433266800000,
-                         1433266860000,
-                         1433266920000,
-                         1433266980000,
-                         1433267040000,
-                         1433267100000,
-                         1433267160000,
-                         1433267220000,
-                         1433267280000,
-                         1433267340000,
-                         1433267400000,
-                         1433267460000,
-                         1433267520000,
-                         1433267580000,
-                         1433267640000,
-                         1433267700000,
-                         1433267760000,
-                         1433267820000,
-                         1433267880000,
-                         1433267940000,
-                         1433268000000,
-                         1433268060000,
-                         1433268120000,
-                         1433268180000,
-                         1433268240000,
-                         1433268300000,
-                         1433268360000,
-                         1433268420000,
-                         1433268480000,
-                         1433268540000]}
-
-    Raises:
-        None
-    """
     def get_metrics(self, **kwargs):
+        """
+        Get the metric data based on the given filters.
+        More info about the metrics API endpoint can be found here:
+        http://help.scoutapp.com/v1.2/docs/api#fetching-metrics
+
+        Args:
+            metric_name (str): the non-humanized metric name
+            metric_type (str): disk|memory|network|process|plugin
+            metric_source_name (str): if plugin, the plugin name. If process, the process name. if disk|network, the volume/interface name
+            environment (str, optional): environment name (exact match required). DEFAULT: no filtering by environment
+            role (str, optional): role name (exact match required.) DEFAULT: no filtering by role
+            server (str, optional): server hostname (exact match, wildcard, or regex ok)
+            duration (str, optional): one of: "30 minutes", "5 hours", "12 hours", "1 day", "1 week", "2 weeks", "1 month", "3 months", "6 months", "1 year". DEFAULT: 5 hours
+            display (str, optional): range|breakout. DEFAULT: range
+
+        Returns:
+            dict: result from the api call in the below format
+
+            {u'metrics': [{u'label': u'hostname1',
+                           u'points': [[0.0, 0.0, 0.0],
+                                       [0.0, 0.0, 0.0],
+                                       [0.0, 0.0, 0.0],
+                                       [0.0, 0.0, 0.0],
+                                       [0.0, 0.0, 0.0],
+                                       [0.0, 0.0, 0.0],
+                                       [0.0, 0.0, 0.0],
+                                       [0.0, 0.0, 0.0],
+                                       [0.0, 0.0, 0.0],
+                                       [0.0, 0.0, 0.0],
+                                       [0.0, 0.0, 0.0],
+                                       [0.0, 0.0, 0.0],
+                                       [0.0, 0.0, 0.0],
+                                       [0.0, 0.0, 0.0],
+                                       [0.0, 0.0, 0.0],
+                                       [0.0, 0.0, 0.0],
+                                       [0.0, 0.0, 0.0],
+                                       [0.0, 0.0, 0.0],
+                                       [0.0, 0.0, 0.0],
+                                       [0.0, 0.0, 0.0],
+                                       [0.0, 0.0, 0.0],
+                                       [0.0, 0.0, 0.0],
+                                       [0.0, 0.0, 0.0],
+                                       [0.0, 0.0, 0.0],
+                                       [0.0, 0.0, 0.0],
+                                       [0.0, 0.0, 0.0],
+                                       [0.0, 0.0, 0.0],
+                                       [0.0, 0.0, 0.0],
+                                       [0.0, 0.0, 0.0],
+                                       [0.0, 0.0, 0.0]],
+                           u'precision': 2,
+                           u'units': u''},
+                          {u'label': u'hostname2',
+                           u'points': [[1.0, 1.0, 1.0],
+                                       [1.0, 1.0, 1.0],
+                                       [1.0, 1.0, 1.0],
+                                       [1.0, 1.0, 1.0],
+                                       [1.0, 1.0, 1.0],
+                                       [1.0, 1.0, 1.0],
+                                       [1.0, 1.0, 1.0],
+                                       [1.0, 1.0, 1.0],
+                                       [1.0, 1.0, 1.0],
+                                       [1.0, 1.0, 1.0],
+                                       [1.0, 1.0, 1.0],
+                                       [1.0, 1.0, 1.0],
+                                       [1.0, 1.0, 1.0],
+                                       [1.0, 1.0, 1.0],
+                                       [1.0, 1.0, 1.0],
+                                       [1.0, 1.0, 1.0],
+                                       [1.0, 1.0, 1.0],
+                                       [1.0, 1.0, 1.0],
+                                       [1.0, 1.0, 1.0],
+                                       [1.0, 1.0, 1.0],
+                                       [1.0, 1.0, 1.0],
+                                       [1.0, 1.0, 1.0],
+                                       [1.0, 1.0, 1.0],
+                                       [1.0, 1.0, 1.0],
+                                       [1.0, 1.0, 1.0],
+                                       [1.0, 1.0, 1.0],
+                                       [1.0, 1.0, 1.0],
+                                       [1.0, 1.0, 1.0],
+                                       [1.0, 1.0, 1.0],
+                                       [1.0, 1.0, 1.0]],
+                           u'precision': 2,
+                           u'units': u''}],
+             u'num_servers': 2,
+             u'step': 60,
+             u'timestamps': [1433266800000,
+                             1433266860000,
+                             1433266920000,
+                             1433266980000,
+                             1433267040000,
+                             1433267100000,
+                             1433267160000,
+                             1433267220000,
+                             1433267280000,
+                             1433267340000,
+                             1433267400000,
+                             1433267460000,
+                             1433267520000,
+                             1433267580000,
+                             1433267640000,
+                             1433267700000,
+                             1433267760000,
+                             1433267820000,
+                             1433267880000,
+                             1433267940000,
+                             1433268000000,
+                             1433268060000,
+                             1433268120000,
+                             1433268180000,
+                             1433268240000,
+                             1433268300000,
+                             1433268360000,
+                             1433268420000,
+                             1433268480000,
+                             1433268540000]}
+
+        Raises:
+            None
+        """
         self.log.info('Getting metrics data based on the filter: {0}'.format(kwargs))
         self.log.debug(kwargs)
         data = self.__query_api('metrics.json', None, kwargs)
@@ -216,24 +216,24 @@ class ScoutAPI(object):
             self.__dump_json_data('get_metrics.json', data.json())
         return data.json()
 
-    """
-    Query the ScoutApp REST API with the given data
-
-    Args:
-        end_point (str): api endpoint to query
-        post_data (str, optional): post request with given data if present, get request if absent
-        query_params (dict, optional): url parameters for get requests
-
-    Returns:
-        requests.models.Response: request response object
-
-    Raises:
-        RuntimeError: API endpoint must not return a 404
-        ValueError: API key must be valid
-        ValueError: query_params must be valid options
-
-    """
     def __query_api(self, end_point, post_data=None, query_params=None):
+        """
+        Query the ScoutApp REST API with the given data
+
+        Args:
+            end_point (str): api endpoint to query
+            post_data (str, optional): post request with given data if present, get request if absent
+            query_params (dict, optional): url parameters for get requests
+
+        Returns:
+            requests.models.Response: request response object
+
+        Raises:
+            RuntimeError: API endpoint must not return a 404
+            ValueError: API key must be valid
+            ValueError: query_params must be valid options
+
+        """
         # Build the full query URL and call the Scout REST API
         full_url = '{0}/{1}/{2}'.format(SCOUT_API_BASE, self.api_key, end_point)
         if post_data:
